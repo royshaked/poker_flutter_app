be5f788ebcd3544ebd87dd97484bdcfb2e7e7843
Roy.sh2001@gmail.com <royshaked@gmail.com>
2026-03-10 03:12:45 +0200

    update

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -15,6 +15,17 @@ subprojects {
     val newSubprojectBuildDir: Directory = newBuildDir.dir(project.name)
     project.layout.buildDirectory.value(newSubprojectBuildDir)
 }
+
+subprojects {
+    afterEvaluate {
+        if (name != "app" && hasProperty("android")) {
+            configure<com.android.build.gradle.LibraryExtension> {
+                compileSdk = 36
+            }
+        }
+    }
+}
+
 subprojects {
     project.evaluationDependsOn(":app")
 }
